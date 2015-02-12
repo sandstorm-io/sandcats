abbc6757df62d1dfbd4158dd50b99a74d204fc95
Asheesh Laroia <asheesh@asheesh.org>
2015-02-12 15:12:46 -0800

    Add non-working test for X-Forwarded-For

--- sandcats/trivial_tests.py
+++ sandcats/trivial_tests.py
@@ -15,3 +15,16 @@ def register_asheesh2_bad_key_type():
          'email': 'asheesh@asheesh.org',
          'pubkey': open('snakeoil-sample-certs/ssl-cert-snakeoil.pem').read()},
         )
+
+def register_asheesh3_x_forwarded_for():
+    # Provide the HTTP_FORWARDED_COUNT=1 environment variable to
+    # Meteor before running this test.
+    #
+    # FIXME: This doesn't pass, but for now, I'm not *that* worried.
+    return requests.post(
+        'http://localhost:3000/register',
+        data={'rawHostname': 'asheesh3',
+         'email': 'asheesh@asheesh.org',
+         'pubkey': open('snakeoil-sample-certs/ssl-cert-snakeoil.pubkey').read()},
+        headers={'X-Forwarded-For': '128.151.2.1'},
+        )
