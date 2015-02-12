eb4a6cf3449f6575395ed7a85870153e38d5e5da
Asheesh Laroia <asheesh@asheesh.org>
2015-02-12 13:17:03 -0800

    Remove unused client code

--- sandcats/sandcats.js
+++ sandcats/sandcats.js
@@ -30,24 +30,6 @@ UserRegistrations.attachSchema(SimpleSchema({
   }
 }));
 
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
 if (Meteor.isServer) {
   BASE_DOMAIN = process.env.BASE_DOMAIN;
   if (! BASE_DOMAIN) {
