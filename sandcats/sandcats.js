5fd4976bb5b79a021ae34fa7c7c82171be8d177b
Asheesh Laroia <asheesh@asheesh.org>
2015-02-11 22:23:09 -0800

    wip

--- sandcats/sandcats.js
+++ sandcats/sandcats.js
@@ -17,6 +17,51 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+
+  function createRecord(mysqlConnection, domain, host, type, content) {
+    mysqlConnection.query(
+      "INSERT INTO records (domain_id, name, type, content) VALUES ((SELECT id FROM domains WHERE domains.name = ?), ?, ?, ?);",
+      [domain, host, type, content],
+      function (err, result) {
+        if (err) throw err;
+
+        console.log("Successfully added " + host + " = " + content + " (" + type + ").");
+      });
+  };
+
+  function formatSoaRecord(primaryNameServer, adminEmailUsingDots, secondsBeforeRefresh,
+                           secondsBeforeRefreshRetry, secondsBeforeDiscardStaleCache,
+                           negativeResultTtl) {
+    return [primaryNameServer, adminEmailUsingDots, secondsBeforeRefresh,
+            secondsBeforeRefreshRetry, secondsBeforeDiscardStaleCache,
+            negativeResultTtl].join(" ");
+  };
+
+  function createDomain(mysqlConnection, domain) {
+    mysqlConnection.query(
+      "INSERT INTO `domains` (name, type) VALUES (?, 'NATIVE');",
+      [domain],
+      function (error, result) {
+        if (error) throw err;
+
+        console.log("Created domain; it received ID #" + result.insertId);
+        console.log("Creating records for top level...");
+        createRecord(mysqlConnection, 'sandcats.io', 'sandcats.io', 'A', '127.0.0.1');
+        createRecord(mysqlConnection, 'sandcats.io', 'www.sandcats.io', 'A', '127.0.0.1');
+        createRecord(mysqlConnection, 'sandcats.io', 'ns1.sandcats.io', 'A', '127.0.0.1');
+        createRecord(mysqlConnection, 'sandcats.io', 'sandcats.io', 'SOA', formatSoaRecord(
+          'ns1.sandcats.io',
+          'admin.sandcats.io',
+          1,
+          60,
+          60,
+          604800,
+          60));
+        console.log("Wow! Success.");
+      });
+
+  };
+
   var pdnsConfig = {
     adapter: 'mysql',
     user: process.env.POWERDNS_USER,
@@ -46,11 +91,11 @@ if (Meteor.isServer) {
         if (err) throw Error(err);
 
         if (rows.length === 0) {
-          console.log("Need to create sandcats.io.");
+          console.log("Creating sandcats.io...");
+          createDomain(connection, "sandcats.io");
         }
       });
 
-    connection.end();
     // code to run on server at startup
   });
 }
