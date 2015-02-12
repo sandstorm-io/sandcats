89e22a252adf6494cf59ae2289eb3f9bb1e2a893
Asheesh Laroia <asheesh@asheesh.org>
2015-02-12 14:56:56 -0800

    Add test validating key format validation

--- sandcats/trivial_tests.py
+++ sandcats/trivial_tests.py
@@ -7,3 +7,11 @@ def register_asheesh():
          'email': 'asheesh@asheesh.org',
          'pubkey': open('snakeoil-sample-certs/ssl-cert-snakeoil.pubkey').read()},
         )
+
+def register_asheesh2_bad_key_type():
+    return requests.post(
+        'http://localhost:3000/register',
+        {'rawHostname': 'asheesh2',
+         'email': 'asheesh@asheesh.org',
+         'pubkey': open('snakeoil-sample-certs/ssl-cert-snakeoil.pem').read()},
+        )
