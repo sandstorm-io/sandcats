78b5ebf9e75aae429c7933939bf5ea7eb82523a7
Asheesh Laroia <asheesh@asheesh.org>
2015-02-11 20:59:55 -0800

    nonsense-wip

--- sandcats/sandcats.js
+++ sandcats/sandcats.js
@@ -17,7 +17,40 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  var pdnsConfig = {
+    adapter: 'mysql',
+    user: process.env.POWERDNS_USER,
+    db: process.env.POWERDNS_DB,
+    password: process.env.POWERDNS_PASSWORD
+  };
+  pdns = Meteor.npmRequire('pdns')(pdnsConfig);
+
   Meteor.startup(function () {
+
+    // Make sure sandcats.io is in the list of domains.
+    var connection = Mysql.createConnection({
+      host: 'localhost',
+      user: process.env.POWERDNS_USER,
+      database: process.env.POWERDNS_DB,
+      password: process.env.POWERDNS_PASSWORD});
+    connection.connect(function(err) {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+
+    connection.query(
+      "SELECT name FROM `domains` WHERE name = ?",
+      ["sandcats.io"],
+      function(err, rows, fields) {
+        if (err) throw Error(err);
+
+        if (rows.length === 0) {
+          console.log("Need to create sandcats.io.");
+        }
+      });
+
+    connection.end();
     // code to run on server at startup
   });
 }
