7c179ea86004aa8ce7ccde5d9ecc47848f35b979
Asheesh Laroia <asheesh@asheesh.org>
2015-02-12 21:19:49 -0800

    Slight protocol change to make clients slightly more DoS resistant

--- sandcats/server/udppings.js
+++ sandcats/server/udppings.js
@@ -4,17 +4,25 @@
 // The protocol is as follows:
 //
 // - Someone using the Sandcats dyndns system will send us a UDP
-//   packet containing their dyndns hostname.
+//   packet containing their dyndns hostname and a 16-byte string
+//   for us to echo.
 //
 // - We look at it, and:
 //
 // - If the hostname matches the IP on file, we do nothing.
 //
-// - If the hostname does not match, we send a reply containing just a
-//   "!".
+// - If the hostname does not match, we send a reply containing their
+//   challenge string.
 //
-// - When the client receives a reply, they know that they should
-//   update the IP address we have on file for them.
+// - When the client receives a reply, and it matches the last
+//   challenge they sent out, then they know that they should update
+//   the IP address we have on file for them.
+//
+// We bother with the challenge to avoid a situation where someone
+// sends a message to every Sandcats client on every UDP port, and
+// causes them to re-register with us. We expect the Sandcats client
+// to generate 16 random ASCII bytes for this, and to let them expire
+// every minute or so.
 //
 // This does mean we perform a MongoDB query for every UDP packet we
 // receive on this port, so we may want to think about making sure the
@@ -24,12 +32,12 @@
 // PowerDNS and its cache, and it can probably deal.
 //
 // Note that if there is no hostname associated with the packet we
-// received, then we do reply. Arguably this leaves us open to sending
-// UDP packets all over the world. I think this "attack" is not a big
-// deal because:
+// received, then we do still reply. Arguably this leaves us open to
+// sending UDP packets all over the world. I think this "attack" is
+// not a big deal because:
 //
 // - There is no traffic amplification. We send one packet per packet
-//   we receive. Plus, our packet has just one byte.
+//   we receive. Plus, our packet is smaller!
 //
 // - I believe that source address spoofing is tough on the 2015-era
 //   Internet. I could be wrong about this!
@@ -58,6 +66,13 @@ startListeningForUdpPings = function() {
 
   var onMessage = Meteor.bindEnvironment(function(err, result) {
     var message = result.message.asciiSlice();
+    var splitted = message.split(" ");
+    var hostname = splitted[0];
+    var challenge = splitted[1];
+    check(challenge, Match.Where(function(x) {
+      return (x.length == 16);
+    }));
+
     var remote = result.remote;
 
     var remoteIp = remote.address;
@@ -66,10 +81,10 @@ startListeningForUdpPings = function() {
     // address and the hostname match should we not reply.
     var weShouldReply = ! UserRegistrations.findOne({
       ipAddress: remoteIp,
-      hostname: message});
+      hostname: hostname});
 
     if (weShouldReply) {
-      server.send(EXCLAMATION_POINT, 0, 1, remote.port, remote.address);
+      server.send(new Buffer(challenge), 0, 16, remote.port, remote.address);
     }
   });
 
