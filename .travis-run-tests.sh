c16f0cda5f17c53e786a2622c3337f4126e0dce4
Asheesh Laroia <asheesh@asheesh.org>
2015-02-26 09:16:23 -0800

    Move Travis-CI test running to separate shell script file
    Also fix mongo:// vs mongodb:// confusion

---
+++ .travis-run-tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set +e  # failure OK for now...
+set -x
+
+pushd /vagrant/sandcats
+MONGO_URL=mongodb://localhost/sandcats_mongo meteor run --settings /etc/sandcats-meteor-settings.json &
+popd
+
+# Wait for Meteor to come online, up to 60 seconds.
+for i in $(seq 60)
+do
+  nc -z localhost 3000
+  retval=$?
+  if [[ $retval == "0" ]]; then
+    echo -n '+'
+    break
+  else
+    sleep 1
+    echo -n '.'
+  fi
+done
+# Make sure anything we prented before is newline-terminated.
+echo
+
+set -e  # Failure is no longer OK!
+
+# Restart nginx, in case it is wants to be all 502-y
+sudo service nginx restart
+
+# Now, actually run the tests
+make action-run-tests
+
